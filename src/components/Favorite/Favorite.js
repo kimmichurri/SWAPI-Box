28791ef8ef81ed1c5271326e4d9e071db671b831
“Kimberly <kimberly.achkio@gmail.com>
2019-03-12 15:59:59 -0600

    Change search container to card container

---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Favorite extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        //declare a counter here for favorite and interpolate below
+        return (
+            <button>view favorites</button>
+        )
+    }
+}
