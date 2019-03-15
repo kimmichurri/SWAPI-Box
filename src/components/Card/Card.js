f80a478573912cedce36cbbfc6c63c56f08d3a13
“Kimberly <kimberly.achkio@gmail.com>
2019-03-14 20:43:20 -0600

    Add more styling for cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-export default function Card({ homeworld, name, population, species}) {
+export default function Card({ homeworld, name, population, species, language}) {
     return (
         <div className="individual-card">
             <h4>{name}</h4>
             <p>{species}</p>
             <p>{homeworld}</p>
             <p>{population}</p>
+            <p>{language}</p>
+            <button>🌟</button>
         </div>
     )
 }
