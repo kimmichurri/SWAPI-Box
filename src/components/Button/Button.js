139ee5f2ca8c57ea38766f60e3d5352987d6e42b
“Kimberly <kimberly.achkio@gmail.com>
2019-03-16 14:13:24 -0600

    Pass method to button component for planets

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Button = ({ getPeople }) => {
+const Button = ({ getPeople, getPlanets }) => {
 
         return (
             <div>
                 <button onClick={getPeople} className="card-category-buttons">people</button>
-                <button className="card-category-buttons">planets</button>
+                <button onClick={getPlanets} className="card-category-buttons">planets</button>
                 <button className="card-category-buttons">vehicles</button>
             </div>
         )
