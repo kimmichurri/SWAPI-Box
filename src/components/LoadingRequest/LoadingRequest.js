24145f2c8321ddd723c3e2bbf2fbe98de02a80c1
“Kimberly <kimberly.achkio@gmail.com>
2019-03-17 00:14:52 -0600

    Complete loading component

--- src/components/LoadingRequest/LoadingRequest.js
+++ src/components/LoadingRequest/LoadingRequest.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import classic_loading_icon from '../../assets/classic_loading_icon.png';
 
 export default function LoadingRequest() {
     return (
-    <div  className="loading-text">
-        <p>Loading...</p>
+    <div>
+        <img
+            className="loading-icon"
+            src={classic_loading_icon}
+            alt="loading icon, the page is loading"
+        />
+        <p className="loading-text">Loading...</p>
     </div>
     )
 }
