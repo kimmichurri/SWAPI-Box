0612ea25d522f1a61e4665bace063e0baaebf0b9
“Kimberly <kimberly.achkio@gmail.com>
2019-03-27 18:17:04 -0600

    Reset tests

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,14 +1,3 @@
 import React from 'react';
 import Button from './Button';
 import { shallow } from 'enzyme';
-
-describe('Button', () => {
-    let wrapper;
-
-    it('should match the snapshot', () => {
-        wrapper = shallow(
-            <Button />
-        )
-        expect(wrapper).toMatchSnapshot()
-    })
-})
