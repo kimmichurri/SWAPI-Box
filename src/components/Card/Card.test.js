67b49cd3f1de175873ba03d8df42792c01e0e71f
“Kimberly <kimberly.achkio@gmail.com>
2019-03-28 13:11:03 -0600

    Update card snapshot

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,3 +1,28 @@
 import React from 'react';
 import Card from './Card';
 import { shallow } from 'enzyme';
+
+describe('Card', () => {
+  let wrapper;
+  let mockCard;
+
+  beforeEach(() => {
+    mockCard= {
+      species: 'Human',
+      homeworld: 'Earth',
+      population: '100',
+      language: 'Spanish',
+      terrain: 'dirt',
+      climate: 'wet',
+      residents: 'me',
+      model: 'jeep',
+      class: 'suv',
+      passengers: '4'
+    }
+    wrapper = shallow(<Card card={mockCard}/>)
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper.debug()).toMatchSnapshot()
+  })
+})
