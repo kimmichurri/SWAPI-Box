543f58c1f98574fd94808b9c4181b312dae35dba
“Kimberly <kimberly.achkio@gmail.com>
2019-03-30 16:22:56 -0600

    Access cards from global props

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Card from '../Card/Card';
 
-export default function CardContainer({ selectedCards }) {
-    console.log(selectedCards)
-    const displayCards = selectedCards.map((card) => (
+export default function CardContainer({ cards }) {
+    console.log(cards)
+    const displayCards = cards.map((card) => (
         <Card key={card.name} card={card} />
     ))
     return (
