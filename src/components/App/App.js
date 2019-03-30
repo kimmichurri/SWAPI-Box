9696e956393aaa748d3ba559a442c0ac1ea337ff
“Kimberly <kimberly.achkio@gmail.com>
2019-03-30 16:22:00 -0600

    Move fetched data to global state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,14 +8,13 @@ import CardContainer from '../CardContainer/CardContainer';
 import { peopleCleaner, findHomeworld, findSpecies, findResidents, planetCleaner, vehicleCleaner } from '../Helper/cleaner';
 import LoadingRequest from '../LoadingRequest/LoadingRequest';
 import { connect } from 'react-redux';
-import { hasError } from '../../actions/index';
+import { hasError, updateData, currentCards } from '../../actions/index';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       openingFilm: {},
-      selectedCards: [],
       favorites: 0,
       loading: false,
     }
@@ -59,7 +58,9 @@ class App extends Component {
       const allPeople = await findHomeworld(data.results)
       const withSpecies = await findSpecies(allPeople)
       const cleanPeopleData = peopleCleaner(withSpecies)
-      this.setState({selectedCards: cleanPeopleData, loading: false})
+      this.props.updateData([...this.props.retrievedData, ...cleanPeopleData])
+      this.props.currentCards(cleanPeopleData)
+      this.setState({loading: false})
     } catch(error) {
       this.props.hasError('There was a problem retrieving the data')
     }
@@ -72,7 +73,9 @@ class App extends Component {
       const data = await this.fetchAnything(url)
       const withResidents = await findResidents(data.results)
       const cleanPlanetData = planetCleaner(withResidents)
-      this.setState({selectedCards: cleanPlanetData, loading: false})
+      this.props.updateData([...this.props.retrievedData, ...cleanPlanetData])
+      this.props.currentCards(cleanPlanetData)
+      this.setState({loading: false})
     } catch(error) {
       this.setState({errorStatus: 'There was a problem retrieving the data'})
     }
@@ -84,14 +87,16 @@ class App extends Component {
       const url = 'https://swapi.co/api/vehicles/'
       const data = await this.fetchAnything(url)
       const cleanVehicleData = await vehicleCleaner(data.results)
-      this.setState({selectedCards: cleanVehicleData, loading: false})
+      this.props.updateData([...this.props.retrievedData, ...cleanVehicleData])
+      this.props.currentCards(cleanVehicleData)
+      this.setState({loading: false})
     } catch(error) {
       this.setState({errorStatus: 'There was a problem retrieving the data'})
     }
   }
 
   render() {
-    const { openingFilm, selectedCards, favorites, loading} = this.state
+    const { openingFilm, favorites, loading} = this.state
     return (
       <div className="app">
           <Favorite favorites={favorites}/>
@@ -105,24 +110,28 @@ class App extends Component {
           ) : (
             <Button getPeople={this.getPeople} getPlanets={this.getPlanets} getVehicles={this.getVehicles}/>
           )
-          } 
+          }  
           </article>
-          {selectedCards.length ? (
-            <CardContainer selectedCards={selectedCards}/>
-          ) : (
+          {this.props.retrievedData.length ? (
+            <CardContainer cards={this.props.retrievedData}/>
+              ) : ( 
             <Scroll openingFilm={openingFilm}/>
-          )}
+           )} 
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  errorStatus : state.errorStatus
+  errorStatus: state.errorStatus,
+  retrievedData: state.retrievedData,
+  currentCards: state.currentCards
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  hasError: (message) => dispatch(hasError(message))
+  hasError: (message) => dispatch(hasError(message)),
+  updateData: (data) => dispatch(updateData(data)),
+  currentCards: (data) => dispatch(currentCards(data))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
