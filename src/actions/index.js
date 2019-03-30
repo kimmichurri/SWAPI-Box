0bb18d35e735a4529e50ac88d6b4bb9554324fe5
“Kimberly <kimberly.achkio@gmail.com>
2019-03-30 16:20:02 -0600

    Add currentCards action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,18 @@ export const hasError = (message) => ({
   type: 'HAS_ERROR',
   message
 })
+
+export const updateData = (data) => ({
+  type: 'UPDATE_DATA',
+  data
+})
+
+export const currentCards = (data) => ({
+  type: 'CURRENT_CARDS',
+  data
+})
+
+// export const addFavorite = (name) => ({
+//   type: 'ADD_FAVORITE',
+//   name
+// })
