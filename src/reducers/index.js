9c069262e70c05344c8de388d7f9d7105ab4b8fc
“Kimberly <kimberly.achkio@gmail.com>
2019-03-30 14:05:36 -0600

    Add errorStatusReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import { errorStatusReducer } from './errorStatusReducer';
 
 const rootReducer = combineReducers({
-
+  errorStatus: errorStatusReducer
 })
 
 export default rootReducer;
