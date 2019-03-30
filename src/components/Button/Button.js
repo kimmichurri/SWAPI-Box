d128ca3eca4c66070d076371f82130a0724a5552
“Kimberly <kimberly.achkio@gmail.com>
2019-03-30 16:24:05 -0600

    Plan out how to only update information that hasn't already been fetched

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const Button = ({ getPeople, getPlanets, getVehicles }) => {
 
+    //getNewCards(category)
+    //if the category does not exist in retrivedData then call the functions below
+    //otherwise just filter out the cards with that category and set them to global currentCards
+
         return (
             <div>
                 <button onClick={getPeople} className="card-category-buttons people">people</button>
@@ -10,5 +14,7 @@ const Button = ({ getPeople, getPlanets, getVehicles }) => {
             </div>
         )
 }
+
+//you will have to mapStateToProps here to access the categories in retrievedData in global state
 
 export default Button
