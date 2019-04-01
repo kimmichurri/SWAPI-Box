d9ca1a32525b87ee9c54159944a8b9af333d0068
“Kimberly <kimberly.achkio@gmail.com>
2019-03-31 22:38:56 -0600

    Remove comments

--- src/components/Helper/fetchAnything.js
+++ src/components/Helper/fetchAnything.js
@@ -1,12 +1,3 @@
-// const fetchAnything = async (url) => {
-//     try {
-//         const response = await fetch(url)
-//         return response.json()
-//     } catch (error) {
-//         throw new Error('Response not okay')
-//     }
-// }
-
 const fetchAnything = async (url) => {
     try {
         const response = await fetch(url)
