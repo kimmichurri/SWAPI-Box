65aa967beaff043325a736171c11730073050a3a
“Kimberly <kimberly.achkio@gmail.com>
2019-04-01 08:50:20 -0600

    Remove comment

--- src/components/Helper/fetchAnything.js
+++ src/components/Helper/fetchAnything.js
@@ -3,7 +3,6 @@ const fetchAnything = async (url) => {
         const response = await fetch(url)
         return response.json()
     } catch (error) {
-        // this.props.errorStatus('There was a problem retrieving the data')
         throw new Error('There was a problem retrieving the data')
     }
 }
